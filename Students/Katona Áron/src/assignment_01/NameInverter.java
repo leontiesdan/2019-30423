72e1e99be21b43604a97edd57c0b2144fb6ba2c8
Katona Áron <katonaaron01@gmail.com>
2019-10-13 10:58:42 +0300

    assingment_01: NameInverter rewritten to handle previously unhandled cases

--- Students/Katona Áron/src/assignment_01/NameInverter.java
+++ Students/Katona Áron/src/assignment_01/NameInverter.java
@@ -5,35 +5,76 @@ import java.util.List;
 
 public class NameInverter {
     public static void main(String[] args) {
-        List<String> names = Arrays.asList(null, "", "SingleWord", "FirstName LastName", " SingleWord ",
-                " FirstName LastName ", "Mr. FirstName LastName", "Mr. Phd. FirstName LastName");
+        List<String> names = Arrays.asList(
+                null,
+                "",
+                "SingleWord",
+                "FirstName LastName",
+                " SingleWord ",
+                " FirstName LastName ",
+                "Mr. FirstName LastName",
+                "Mr. Phd. FirstName LastName",
+                "Dr. Joe",
+                "Dr.         Phd. Jack",
+                "Jack John Bill",
+                "Dr. Phd. Jack John Bill",
+                "Dr. Phd."
+        );
         for (String name : names) {
             System.out.println("\"" + name + "\" inverted is: \"" + invertName(name) + "\"");
 
         }
     }
 
+    /**
+     * Inverts a given name, such that:
+     * null -> null
+     * SingleName -> SingleName
+     * FirstName LastName -> LastName, FirstName
+     * Title1. Title2. FirstName -> Title1. Title2. FirstName
+     * Title1. Title2. FirstName LastName -> LastName, FirstName Title1. Title2.
+     * Title1. Title2. FirstName MiddleName1 MiddleName2 LastName -> LastName, FirstName MiddleName1 MiddleName2 Title1. Title2.
+     *
+     * @param name The name to be inverted
+     * @return The inverted name
+     */
     public static String invertName(String name) {
         if (name == null) {
             return null;
         }
 
-        name = name.trim();
-        if (name.isEmpty()) {
-            return "";
+        int firstNameIndex = -1, lastNameIndex = -1;
+        StringBuilder title = new StringBuilder(), invertedName = new StringBuilder();
+
+        name = name.trim().replaceAll(" +", " ");
+        String[] words = name.split(" ");
+
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].endsWith(".")) {
+                if (title.length() != 0) {
+                    title.append(" ");
+                }
+                title.append(words[i]);
+            } else {
+                if (firstNameIndex == -1) {
+                    firstNameIndex = lastNameIndex = i;
+                } else {
+                    lastNameIndex = i;
+                }
+            }
         }
-        if (!name.contains(" ")) {
+        if (lastNameIndex <= firstNameIndex) {
             return name;
         }
 
-        int nameSeparatorIndex = name.lastIndexOf(" ");
-        int titleEndIndex = name.lastIndexOf(" ", nameSeparatorIndex - 1);
-        StringBuilder sb = new StringBuilder(name.substring(nameSeparatorIndex + 1))
-                .append(", ")
-                .append(name, titleEndIndex + 1, nameSeparatorIndex);
-        if (titleEndIndex > -1) {
-            sb.append(" ").append(name, 0, titleEndIndex);
+        invertedName.append(words[lastNameIndex]).append(",");
+        for (int i = firstNameIndex; i < lastNameIndex; i++) {
+            invertedName.append(" ").append(words[i]);
         }
-        return sb.toString();
+
+        if (title.length() != 0) {
+            invertedName.append(" ").append(title);
+        }
+        return invertedName.toString();
     }
 }
--- Students/Katona Áron/src/assignment_01/TestNameInverter.java
+++ Students/Katona Áron/src/assignment_01/TestNameInverter.java
@@ -16,5 +16,17 @@ public class TestNameInverter {
         Assertions.assertEquals(NameInverter.invertName("Mr. FirstName LastName"), "LastName, FirstName Mr.");
         Assertions.assertEquals(NameInverter.invertName("Mrs. FirstName LastName"), "LastName, FirstName Mrs.");
         Assertions.assertEquals(NameInverter.invertName("Mr. Phd. FirstName LastName"), "LastName, FirstName Mr. Phd.");
+        //Last name is missing
+        Assertions.assertEquals(NameInverter.invertName("Mr. Phd. FirstName"), "Mr. Phd. FirstName");
+        //Only title was given
+        Assertions.assertEquals(NameInverter.invertName("Mr. Phd."), "Mr. Phd.");
+        //Additional spaces between words
+        Assertions.assertEquals(NameInverter.invertName(" Mr.  Phd.  FirstName  LastName "), "LastName, FirstName Mr. Phd.");
+        //Multiple names
+        Assertions.assertEquals(NameInverter.invertName("Mr. Phd. FirstName MiddleName1 MiddleName2 LastName"),
+                "LastName, FirstName MiddleName1 MiddleName2 Mr. Phd.");
+        Assertions.assertEquals(NameInverter.invertName("FirstName MiddleName1 MiddleName2 LastName"),
+                "LastName, FirstName MiddleName1 MiddleName2");
+
     }
 }
