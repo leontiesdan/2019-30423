3918e76dd2c4881881c45d3411f6dd33b9d2ed43
oana <oanamirimus@gmail.com>
2019-10-21 19:35:27 +0300

    hotel room...

---
+++ Students/Irimus Oana Mara/HotelRoom/src/HotelRooms/Room.java
@@ -0,0 +1,12 @@
+package HotelRooms;
+
+public class Room {
+	Integer number;
+	Integer area;
+
+	public Room(Integer number, Integer area) {
+		
+		this.number = number;
+		this.area = area;
+	}
+}
